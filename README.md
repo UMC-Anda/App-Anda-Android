86433ae738f1f91cda2b030c154bd441ea0a6cf8
Yackak <julieteoh1@sju.ac.kr>
2022-07-11 04:27:56 +0900

    회원가입 템플릿 적용

--- README.md
+++
@@ -1,3 +0,0 @@
-KAU Astro Chaser Web server 
-yackak test
-yackak desktop test commit
---
+++ app/src/main/java/com/example/udemy_android_template/data/entities/User.kt
@@ -0,0 +1,13 @@
+package com.example.udemy_android_template.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("nickName") val nickName: String,
+    @SerializedName("realName") val realName: String,
+    @SerializedName("phoneNumber") val phoneNumber: String,
+    @SerializedName("dateofBirth") val dateOfBirth: String,
+    @SerializedName("recommander") val recommander: String?
+)
